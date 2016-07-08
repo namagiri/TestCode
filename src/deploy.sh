80292da23800b04baa5c272d89462ef10bb87717
Vishal Sharma <vishalsharma.iitm10@gmail.com>
2016-07-09 01:22:47 +0530

    chanage in deploy.sh file

--- src/deploy.sh
+++ src/deploy.sh
@@ -4,4 +4,4 @@ docker pull svishal/testcode
 docker stop testcode
 docker rm testcode
 docker rmi svishal/testcode
-docker run -d --name testcode -p 8080:8080 svishal/testcode
+docker run -d --name testcode --restart always -p 8080:8080 svishal/testcode
